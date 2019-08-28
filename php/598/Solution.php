1dfac08538a40dd5b8e1d9acf7cb555f0e012b47
shuyh <nelson.shu.chn@gmail.com>
2019-08-28 16:01:34 +0800

    范围求和 II

---
+++ php/598/Solution.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: shuyu
+ * Date: 2019/8/28
+ * Time: 15:57
+ */
+
+/**
+ * 
+ * Class Solution
+ */
+class Solution
+{
+    /**
+     * @param Integer $m
+     * @param Integer $n
+     * @param Integer[][] $ops
+     * @return Integer
+     */
+    function maxCount($m, $n, $ops) {
+        if(empty($ops)){
+            return $m * $n;
+        }
+        $allIs = array_column($ops, 0);
+        $allJs = array_column($ops, 1);
+
+        return min($allIs) * min($allJs);
+    }
+}
